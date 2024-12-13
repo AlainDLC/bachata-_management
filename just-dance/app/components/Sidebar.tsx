1f342628b410a404e3c12adcfa9f81d43fdc0c75
AlainDLC <jd_alain@hotmail.com>
2024-12-13 14:04:05 +0100

    change bg color

--- just-dance/app/components/Sidebar.tsx
+++ just-dance/app/components/Sidebar.tsx
@@ -5,9 +5,9 @@ export default function Sidebar() {
     <div className="bg-gray-800 text-white w-64 h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Kurser</h1>
       <ul>
-        <li className="mb-2 cursor-pointer">Kurs 1</li>
-        <li className="mb-2 cursor-pointer">Kurs 2</li>
-        <li className="mb-2 cursor-pointer">Kurs 3</li>
+        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 1</li>
+        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 2</li>
+        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 3</li>
       </ul>
     </div>
   );
