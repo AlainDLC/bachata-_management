9da1e63ab9aaf13719b9e55f4113b34f20b17665
AlainDLC <jd_alain@hotmail.com>
2024-12-14 20:12:48 +0100

    change css

--- just-dance/app/components/Sidebar.tsx
+++ just-dance/app/components/Sidebar.tsx
@@ -1,14 +1,28 @@
+import Link from "next/link";
 import React from "react";
-
-export default function Sidebar() {
+import { PowerIcon } from "@heroicons/react/24/solid";
+export default function SideBar() {
   return (
-    <div className="bg-gray-800 text-white w-64 h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Kurser</h1>
-      <ul>
-        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 1</li>
-        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 2</li>
-        <li className="mb-2 cursor-pointer hover:bg-green-400">Kurs 3</li>
-      </ul>
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40"></div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <div className=" h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form
+          action={async () => {
+            "use server";
+          }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="">Sign Out</div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
